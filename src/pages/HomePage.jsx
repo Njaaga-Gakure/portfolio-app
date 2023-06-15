6f6c14b1e8a24acb39c97f21356c3d006e9195db
Njaaga-Gakure <50949648+Njaaga-Gakure@users.noreply.github.com>
2023-06-15 09:30:43 +0300

    Edit footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
         <div className="hero-center">
           <h1 className="hero-title">hi, i'm <span>Brian Gakure</span></h1>
           <p className="hero-text">
-          Passionate self-taught JavaScript developer adept at both front-end and back-end development. Eager to contribute to impactful projects and enhance professional skills. Open to opportunities from potential employers. Let's connect!
+          Passionate self-taught JavaScript developer adept at front-end using Reactjs. Eager to contribute to impactful projects and enhance professional skills. Open to opportunities from potential employers. Let's connect!
           </p>
           <Link className="hero-btn" to="/projects">explore projects</Link>
         </div>
